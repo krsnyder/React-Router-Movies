cc8f1a13a85414d9a6ef7657d7800f9bb7cc6ea4
Kirk <kirkrobertsnyder@gmail.com>
2021-02-22 16:20:45 -0500

    Set up Routes and directed card links to proper URLs

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Route, Link, Switch} from 'react-router-dom'
+
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'
 
 export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+  // const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
+        .get('http://localhost:5000/api/movies')
         .then(response => {
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data)
+          // console.log(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -29,8 +34,18 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
+      
+      <Switch>
+        <Route path="/movies/:id" movie={movieList}>
+          <Movie movie={movieList}/>
+        </Route>
 
-      <div>Replace this Div with your Routes</div>
+        <Route path="/">
+          <MovieList movies={movieList} />
+        </Route>
+      </Switch>
+      
     </div>
+
   );
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useParams, NavLink, useRouteMatch, Link} from 'react-router-dom'
 
 export default function MovieList(props) {
+  const { url } = useRouteMatch();
+  const { id } = useParams();
+
+  // const movie = props.movies.find((movie) => {
+  //   return movie.id == id;
+  // }) || {};
+
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <div key={movie.id}>
+          <Link to={`${url}${movie.id}`}>
+            <MovieDetails key={movie.id} movie={movie} />
+          </Link>
+        </div>
       ))}
     </div>
   );
